078ebec252b4c3defd789684874bccc976f936f7
PaccyC <abayisengaaimepacifique@gmail.com>
2024-09-10 06:19:45 +0200

    feat:created navlinks file

---
+++ src/utils/navLinks.ts
@@ -0,0 +1,28 @@
+export const navLinks=[
+    
+    {
+    "label":"Home",
+    "icon":"",
+    "route":"/"
+    },
+    {
+        "label":"Messages",
+        "icon":"",
+        "route":"/messages"
+    },
+    {
+       "label":"Tasks",
+        "icon":"",
+        "route":"/tasks"
+    },
+    {
+        "label":"Members",
+        "icon":"",
+        "route":"/members"
+     },
+    {
+        "label":"Settings",
+        "icon":"",
+        "route":"/settings"
+    }              
+]
