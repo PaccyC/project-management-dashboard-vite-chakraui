b275ad32f31207301bc65c48238a29e0a3d04c30
PaccyC <abayisengaaimepacifique@gmail.com>
2024-09-11 20:08:22 +0200

    feat: finished the main content of the dashboard

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -6,7 +6,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 const ProjectDetails = () => {
   return (
     <div className=" w-full grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div className=" h-[650px] w-[400px] bg-secondary rounded-lg flex flex-col gap-4  px-4">
+        <div className=" h-[660px] w-[400px] bg-secondary rounded-lg flex flex-col gap-4  px-4 py-3">
            <div className=" flex w-full justify-between py-3">
              <div className=" flex gap-2 items-center">
                 <GoDotFill className=" text-light-1"/>
@@ -23,7 +23,7 @@ const ProjectDetails = () => {
              <hr  className=" bg-light-1 h-1 w-full"/>
            </div>
            {/* Cards */}
-           <div className=" w-full h-[195px] bg-white rounded-lg px-3 py-4 ">
+           <div className=" w-full h-[195px] bg-white rounded-lg px-3 py-4 shadow-lg ">
             <div className=" flex flex-col space-y-4">
                 <div className=" w-full flex flex-col gap-2">
                     <div className=" flex justify-between w-full">
@@ -86,7 +86,7 @@ const ProjectDetails = () => {
                     </div>
             </div> 
            </div>
-           <div className=" w-full h-[195px] bg-white rounded-lg px-3 py-4 ">
+           <div className=" w-full h-[195px] bg-white rounded-lg px-3 py-4 shadow-lg ">
             <div className=" flex flex-col space-y-4">
                 <div className=" w-full flex flex-col gap-2">
                     <div className=" flex justify-between w-full">
@@ -149,7 +149,7 @@ const ProjectDetails = () => {
                     </div>
             </div> 
            </div>
-           <div className=" w-full h-[195px] bg-white rounded-lg px-3 py-4 ">
+           <div className=" w-full h-[195px] bg-white rounded-lg px-3 py-4 shadow-lg">
             <div className=" flex flex-col space-y-4">
                 <div className=" w-full flex flex-col gap-2">
                     <div className=" flex justify-between w-full">
@@ -213,7 +213,7 @@ const ProjectDetails = () => {
             </div> 
            </div>
         </div>
-        <div className=" h-[650px] w-[400px] bg-secondary rounded-lg flex flex-col gap-4  px-4">
+        <div className=" h-[660px] w-[400px] bg-secondary rounded-lg flex flex-col gap-4  px-4 py-3">
             <div className=" flex w-full justify-between py-3">
                 <div className=" flex gap-2 items-center">
                     <GoDotFill className=" text-orange-500"/>
@@ -231,16 +231,142 @@ const ProjectDetails = () => {
 
             </div>
 
-            <div className="w-full h-[258px] bg-white rounded-lg px-3 py-4">
+            <div className="w-full h-[258px] bg-white rounded-lg px-3 py-4 shadow-lg">
+                <div className=" flex flex-col space-y-4">
+                    <div className=" w-full flex flex-col gap-2">
+                            <div className=" flex justify-between w-full">
+                                <Badge variant="outline" className=" text-sm text-orange-500 bg-[#DFA874] font-thin">LOW</Badge>
+                                <BsThreeDots/>
+                                
+                            </div>
+                            <h3 className=" font-semibold text-dark">Onboarding Illustrations</h3>
+                            <img 
+                              src="/assets/image/card-image-1.jpg" 
+                              alt="image 1" />
+                        </div> 
 
+                        {/* people, comments,files */}
+                    <div className=" flex justify-between w-full">
+                       <div className="flex -space-x-4">
+                            {/* First Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 1" />
+                                <AvatarFallback>AB</AvatarFallback>
+                            </Avatar>
+
+                            {/* Second Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 2" />
+                                <AvatarFallback>CD</AvatarFallback>
+                            </Avatar>
+
+                            {/* Third Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 3" />
+                                <AvatarFallback>EF</AvatarFallback>
+                            </Avatar>
+
+    
+                         </div>
+
+                         <div className=" flex gap-3">
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/comment.svg" 
+                              alt="comment"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">14 comments</p>
+                            </div>
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/file.svg" 
+                              alt="file"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">15 files</p>
+                            </div>
+                            <div>
+
+                            </div>
+ 
+                         </div>
+
+                    </div>
+                    </div>
             </div>
-            <div className="w-full h-[258px] bg-white rounded-lg px-3 py-4">
+            <div className="w-full h-[258px] bg-white rounded-lg px-3 py-4 shadow-lg">
+                <div className=" flex flex-col space-y-4">
+                    <div className=" w-full flex flex-col gap-2">
+                            <div className=" flex justify-between w-full">
+                                <Badge variant="outline" className=" text-sm text-orange-500 bg-[#DFA874] font-thin">LOW</Badge>
+                                <BsThreeDots/>
+                                
+                            </div>
+                            <h3 className=" font-semibold text-dark">Moodboard</h3>
+                            <div className=" flex justify-between">
+                                <img 
+                                src="/assets/image/card-image-2.jpg" 
+                                alt="image 1"
+                                width={160}
+                                height={79}
+                                />
+                                 <img 
+                                src="/assets/image/card-image-3.jpg" 
+                                alt="image 1"
+                                width={160}
+                                height={79}
+                                />
+                            </div>  
+                        </div> 
 
+                        {/* people, comments,files */}
+                    <div className=" flex justify-between w-full">
+                       <div className="flex -space-x-4">
+                            {/* First Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 1" />
+                                <AvatarFallback>AB</AvatarFallback>
+                            </Avatar>
+
+
+    
+                         </div>
+
+                         <div className=" flex gap-3">
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/comment.svg" 
+                              alt="comment"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">9 comments</p>
+                            </div>
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/file.svg" 
+                              alt="file"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">10 files</p>
+                            </div>
+                            <div>
+
+                            </div>
+ 
+                         </div>
+
+                    </div>
+                    </div>
             </div>
 
         </div>
-        <div className=" h-[650px] w-[400px] bg-secondary rounded-lg flex flex-col gap-4  px-4">
-        <div className=" flex w-full justify-between py-3">
+        <div className=" h-[660px] w-[400px] bg-secondary rounded-lg flex flex-col gap-4  px-4 py-3">
+            <div className=" flex w-full justify-between py-3">
                 <div className=" flex gap-2 items-center">
                     <GoDotFill className=" text-blue-400"/>
                     <p className=" text-dark">Done</p>
@@ -255,6 +381,134 @@ const ProjectDetails = () => {
             <div>
 
                <hr  className=" bg-green-600 h-1 w-full"/>
+             
+            </div>
+
+            <div className="w-full h-[400px] bg-white rounded-lg px-3 py-4 shadow-lg">
+                <div className=" flex flex-col space-y-4">
+                    <div className=" w-full flex flex-col gap-2">
+                            <div className=" flex justify-between w-full">
+                                <Badge variant="outline" className=" text-sm text-[#68B266] bg-[#83C29D] font-thin">Completed</Badge>
+                                <BsThreeDots/>
+                                
+                            </div>
+                            <h3 className=" font-semibold text-dark">Mobile App Design</h3>
+                            <img 
+                              src="/assets/image/card-image-4.jpg" 
+                              alt="image 1" />
+                        </div> 
+
+                        {/* people, comments,files */}
+                    <div className=" flex justify-between w-full">
+                       <div className="flex -space-x-4">
+                            {/* First Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 1" />
+                                <AvatarFallback>AB</AvatarFallback>
+                            </Avatar>
+
+                            {/* Second Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 2" />
+                                <AvatarFallback>CD</AvatarFallback>
+                            </Avatar>
+
+                         
+
+    
+                         </div>
+
+                         <div className=" flex gap-3">
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/comment.svg" 
+                              alt="comment"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">12 comments</p>
+                            </div>
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/file.svg" 
+                              alt="file"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">15 files</p>
+                            </div>
+                            <div>
+
+                            </div>
+ 
+                         </div>
+
+                    </div>
+                    </div>
+            </div>
+            <div className="w-full h-[195px] bg-white rounded-lg px-3 py-4 shadow-lg">
+                <div className=" flex flex-col space-y-4">
+                    <div className=" w-full flex flex-col gap-2">
+                            <div className=" flex justify-between w-full">
+                                <Badge variant="outline" className=" text-sm text-[#68B266] bg-[#83C29D] font-thin">Completed</Badge>
+                                <BsThreeDots/>
+                                
+                            </div>
+                            <h3 className=" font-semibold text-dark">Design system</h3>
+                           <p className="text-primary text-sm">It just needs to adapt the UI from what you did before </p>
+                        </div> 
+
+                        {/* people, comments,files */}
+                    <div className=" flex justify-between w-full">
+                       <div className="flex -space-x-4">
+                            {/* First Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 1" />
+                                <AvatarFallback>AB</AvatarFallback>
+                            </Avatar>
+
+                            {/* Second Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 2" />
+                                <AvatarFallback>CD</AvatarFallback>
+                            </Avatar>
+
+                            {/* Third Avatar */}
+                            <Avatar className="w-6 h-6 border-2 border-white">
+                                <AvatarImage src="/assets/image/profile.jpg" alt="User 3" />
+                                <AvatarFallback>EF</AvatarFallback>
+                            </Avatar>
+
+    
+                         </div>
+
+                         <div className=" flex gap-3">
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/comment.svg" 
+                              alt="comment"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">14 comments</p>
+                            </div>
+                            <div className=" flex gap-1 items-center">
+                            <img 
+                              src="/assets/icons/file.svg" 
+                              alt="file"
+                              width={16}
+                              height={16}
+                              />
+                              <p className="text-primary text-xs">15 files</p>
+                            </div>
+                            <div>
+
+                            </div>
+ 
+                         </div>
+
+                    </div>
+                    </div>
             </div>
 
         </div>
